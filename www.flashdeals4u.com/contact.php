7e6a50553554e17544d09ffc2a3d5e033820c6b4
Your Name <you@example.com>
2021-12-11 00:52:27 +0400

    my files

---
+++ www.flashdeals4u.com/contact.php
@@ -0,0 +1,23 @@
+
+<?php
+	echo "<script>
+             alert('Message sent Succesfully');
+			 alert('Thank you for contacting Us');  
+             window.history.go(-1);
+     </script>";
+
+    $name       = @trim(stripslashes($_POST['name'])); 
+    $email      = @trim(stripslashes($_POST['email']));
+    $subject    = @trim(stripslashes($_POST['subject']));   
+    $message    = @trim(stripslashes($_POST['message'])); 
+
+    $email_from = $email;
+    $email_to = 'amjadkiyemba@yahoo.com';//replace with your email
+
+    $body = 'Name: ' . $name . "\n\n" . 'Email: ' . $email . "\n\n" . 'Subject' . $subject "\n\n" . 'Message: ' . $message;
+
+    $success = @mail($email_to, $subject, $body, 'From: <'.$email_from.'>');
+
+    echo json_encode($status);
+    die;
+	?>
